e7b1d9338b3c4add5c0499d769a9130eddf26a87
JD037 <joj237@gmail.com>
2023-01-28 14:18:41 -0800

    3.Task 1st commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in upper and lower case.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int character;
+
+	character = 'a';
+	while (character <= 'z')
+	{
+		putchar(character);
+		character++;
+	}
+
+	character = 'A';
+	while (character <= 'Z')
+	{
+		putchar(character);
+		character++;
+	}
+
+	putchar('\n');
+	return (0);
+}
