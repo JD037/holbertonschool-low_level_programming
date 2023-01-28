2ad776fd6783ee4dba7e1a37f3050076b57398a1
JD037 <joj237@gmail.com>
2023-01-28 15:47:41 -0800

    7.Task 1st commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in reverse
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int character;
+
+	character = 'z';
+	while (character >= 'a')
+	{
+		putchar(character);
+		character--;
+	}
+	putchar('\n');
+	return (0);
+}
+
