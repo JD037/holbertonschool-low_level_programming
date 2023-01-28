07391834f6a60c0f5c1de96c936a4e17d9f45f6d
JD037 <joj237@gmail.com>
2023-01-28 15:11:08 -0800

    5.Task 1st commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - displays zero through nine.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
+
