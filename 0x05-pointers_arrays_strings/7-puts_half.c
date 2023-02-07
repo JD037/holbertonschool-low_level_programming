be6466db5a2151af4972bd303cf47352e51303cd
JD037 <joj237@gmail.com>
2023-02-06 16:20:13 -0800

    7.Task 1st commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - print second half of string
+ * @str: string to print second half of
+ */
+void puts_half(char *str)
+{
+	int i, length;
+
+	for (length = 0; str[length] != '\0'; length++)
+		;
+
+	if (length % 2 == 0)
+	{
+		for (i = length / 2; str[i] != '\0'; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		for (i = (length - 1) / 2; str[i] != '\0'; i++)
+			_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
