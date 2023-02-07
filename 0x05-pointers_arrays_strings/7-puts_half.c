04a98ff85c3f91f7c5815a0a5ee2e7d49dffcc92
JD037 <joj237@gmail.com>
2023-02-06 16:29:58 -0800

    7.Task 2nd commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,21 +5,28 @@
  */
 void puts_half(char *str)
 {
-	int i, length;
+	int i;
+	int length = 0;
 
-	for (length = 0; str[length] != '\0'; length++)
-		;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		length++;
+	}
 
 	if (length % 2 == 0)
 	{
-		for (i = length / 2; str[i] != '\0'; i++)
+		for (i = length / 2; i < length; i++)
+		{
 			_putchar(str[i]);
+		}
 	}
 	else
 	{
-		for (i = (length - 1) / 2; str[i] != '\0'; i++)
+		for (i = (length - 1) / 2; i < length; i++)
+		{
 			_putchar(str[i]);
+		}
 	}
 
-	_putchar('\n');
+	_putchar('\n')
 }
