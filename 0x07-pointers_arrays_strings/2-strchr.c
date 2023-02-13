ff54b3da9114317fc7123af3af9913f71bbfe394
JD037 <joj237@gmail.com>
2023-02-13 12:30:20 -0800

    2.Task redo 3rd commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - locate a character in a string
@@ -20,5 +21,5 @@ char *_strchr(char *s, char c)
 		}
 	}
 
-	return ('\0');
+	return (NULL);
 }
