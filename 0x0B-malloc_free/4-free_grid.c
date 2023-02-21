842fd9feb5b7b23604a25da9d1fcf96457aba9a6
JD037 <joj237@gmail.com>
2023-02-20 16:30:31 -0800

    4.Task 2nd commit

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * free_grid - frees a 2 dimensional grid previously created by 
+ * free_grid - frees a 2 dimensional grid previously created by
  * alloc_grid function.
  * @grid: the 2 dimensional grid to be freed.
  * @height: height of the grid
