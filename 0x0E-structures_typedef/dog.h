d52b4ee69553bdf16abd415cd8db36e0d27fae0a
JD037 <joj237@gmail.com>
2023-03-02 11:49:39 -0800

    0.Task 1st commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct representing a dog
+ * @name: the name of the dog (string)
+ * @age: the age of the dog (float)
+ * @owner: the name of the dog's owner (string)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
