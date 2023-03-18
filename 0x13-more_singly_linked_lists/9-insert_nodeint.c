a3da77d8c8190ac046204b89eda380bbadb5ed85
JD037 <joj237@gmail.com>
2023-03-18 10:21:14 -0700

    added 3-function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -69,3 +69,36 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	return (new_node);
 }
+
+/**
+ * add_nodeint_end - adds a new node at the end of a listint_t list.
+ * @head: pointer to pointer to the head of the list
+ * @n: integer to be added to the list
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node, *tmp;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	tmp = *head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = new_node;
+
+	return (new_node);
+}
