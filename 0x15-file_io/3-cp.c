f3ef6b5c8d54559905b90a1d231dc2550a890e07
JD037 <joj237@gmail.com>
2023-03-19 13:39:04 -0700

    program that copies the content of a file to another file.

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+#define BUFFER_SIZE 1024
+
+/**
+ * error - Print an error message and exit the program with a given code
+ *
+ * @msg: The error message to print
+ * @file: The name of the file causing the error
+ * @code: The exit code to use
+ */
+void error(char *msg, char *file, int code)
+{
+	dprintf(STDERR_FILENO, msg, file);
+	exit(code);
+}
+
+/**
+ * main - Copy the contents of one file to another file
+ *
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: 0 on success, 97-100 on failure
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, n_read, n_write;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+		error("Usage: %s file_from file_to\n", argv[0], 97);
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		error("Error: Can't read from file %s\n", argv[1], 98);
+
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+		error("Error: Can't write to %s\n", argv[2], 99);
+
+	while ((n_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		n_write = write(fd_to, buffer, n_read);
+		if (n_write == -1)
+			error("Error: Can't write to %s\n", argv[2], 99);
+	}
+
+	if (n_read == -1)
+		error("Error: Can't read from file %s\n", argv[1], 98);
+
+	if (close(fd_from) == -1)
+		error("Error: Can't close fd %d\n", fd_from, 100);
+
+	if (close(fd_to) == -1)
+		error("Error: Can't close fd %d\n", fd_to, 100);
+
+	return (0);
+}
