6dff48137af7e87d68ddd778e6c5aa9e7d863ba7
JD037 <joj237@gmail.com>
2023-03-19 11:43:57 -0700

    changed prt binary to keep track of 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,12 +9,16 @@ void print_binary(unsigned long int n)
 {
 	unsigned long int mask = 1;
 	int bit_count = sizeof(unsigned long int) * 8;
+	int found_one = 0;
 
 	while (bit_count--)
 	{
 		if ((n & (mask << bit_count)) != 0)
+		{
 			_putchar('1');
-		else if (bit_count == 0 || _putchar('0'))
-			continue;
+			found_one = 1;
+		}
+		else if (found_one || bit_count == 0)
+			_putchar('0');
 	}
 }
