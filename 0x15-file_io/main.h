a6bac719c8b6a6cf4cf5ab6041ed6c94d8aaac12
JD037 <joj237@gmail.com>
2023-03-20 16:16:18 -0700

    changed back cp and added prototypes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
+void error(const char *msg, char *file, int code);
 
 #endif /* MAIN_H */
