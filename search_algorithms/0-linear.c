a4eba3feb4f0c2f3d80c8a3737317834872bd3e0
Josh D <joj237@gmail.com>
2023-12-17 13:48:36 -0800

    fix indentation issues with betty

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -7,21 +7,22 @@
  * @size: Number of elements in array
  * @value: Value to search for
  *
- * Return: First index where value is located, or -1 if not found or array is NULL
+ * Return: First index where value is located, or -1
+ * if not found or array is NULL
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
+	size_t i;
 
-    if (!array)
-        return (-1);
+	if (!array)
+		return (-1);
 
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-    }
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
 
-    return (-1);
+	return (-1);
 }
